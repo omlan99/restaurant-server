cafc9c16197493bbba7694657e5d63ad09bc0367
omlan99 <omlan10616@gmail.com>
2025-01-15 03:47:32 +0600

    created server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) =>{
+    res.send('Data is coming')
+})
+
+app.listen(port, () =>{
+    console.log('app running on port :', port)
+})
