ac850c01eb79f4eadb72d5118a55efaba7fbeaf0
omlan99 <omlan10616@gmail.com>
2025-01-18 20:56:48 +0600

    added delete user api

--- index.js
+++ index.js
@@ -32,9 +32,14 @@ async function run() {
     const reviewCollection = client.db('MenuDB').collection('reviews')
     const cartCollection = client.db('MenuDB').collection('carts')
 
+    app.get('/users', async(req,res) =>{
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
     app.post('/users', async(req,res) => {
       const  user = req.body;
-      
+
       const query = {email : user.email}
       const existingUser = await userCollection.findOne(query);
       if(existingUser){
@@ -42,6 +47,14 @@ async function run() {
       }
       const result = await userCollection.insertOne(user)
       res.send(result)  
+    })
+
+    app.delete('/users/:id', async(req,res) =>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      console.log(query)
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
     })
 
     app.get('/menu', async (req,res) => {
