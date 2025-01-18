7d035ccd60bda590993c60fd38709eea5cd95347
omlan99 <omlan10616@gmail.com>
2025-01-18 23:13:10 +0600

    added jwt api

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
+const cors = require("cors");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.e6udf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -19,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,93 +26,103 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const userCollection = client.db('MenuDB').collection('user')
-    const menuCollection = client.db('MenuDB').collection('menus')
-    const reviewCollection = client.db('MenuDB').collection('reviews')
-    const cartCollection = client.db('MenuDB').collection('carts')
+    const userCollection = client.db("MenuDB").collection("user");
+    const menuCollection = client.db("MenuDB").collection("menus");
+    const reviewCollection = client.db("MenuDB").collection("reviews");
+    const cartCollection = client.db("MenuDB").collection("carts");
 
-    app.get('/users', async(req,res) =>{
-      const result = await userCollection.find().toArray()
-      res.send(result)
-    })
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user)
+      const token = jwt.sign(user, process.env.access_token,{expiresIn : '1h'});
+      console.log(token)
+      res.send(token)
+    });
 
-    app.post('/users', async(req,res) => {
-      const  user = req.body;
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
-      const query = {email : user.email}
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email };
       const existingUser = await userCollection.findOne(query);
-      if(existingUser){
-        return res.send({message : 'user already exist', inseertedId : null })
+      if (existingUser) {
+        return res.send({ message: "user already exist", inseertedId: null });
       }
-      const result = await userCollection.insertOne(user)
-      res.send(result)  
-    })
-    app.patch('/users/admin/:id', async(req,res) => {
-      const id = req.params.id
-      const filter = {_id : new ObjectId(id)}
-      const updatedDoc = {$set:{
-        role : 'admin'
-      }}
-      const result = await userCollection.updateOne(filter, updatedDoc)
-      res.send(result)
-    })
-    app.delete('/users/:id', async(req,res) =>{
-      const id = req.params.id
-      const query = {_id : new ObjectId(id)}
-      const result = await userCollection.deleteOne(query)
-      res.send(result)
-    })
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
-    app.get('/menu', async (req,res) => {
-        const result = await menuCollection.find().toArray()
-        res.send(result)
-    })
-    app.get('/reviews', async (req, res) => {
-        const result = await reviewCollection.find().toArray();
-        res.send(result)
-    })
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
 
     //cart item
-    app.get('/carts', async (req, res) =>{
-      const email = req.query.email
-      const query = {email : email}
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
       const result = await cartCollection.find(query).toArray();
-      res.send(result)
-
-    })
-    app.post('/carts', async (req,res) => {
-      const cartItem = req.body
+      res.send(result);
+    });
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.delete('/delete/:id', async (req,res)=>{
-      const id = req.params.id
-      const query = {_id : new ObjectId(id)}
-      const result = await cartCollection.deleteOne(query)
-      res.send(result)
-    })
+    app.delete("/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
 
-  
-
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await newFunction();
   }
 
-    async function newFunction() {
-        await client.close();
-    }
+  async function newFunction() {
+    await client.close();
+  }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) =>{
-    res.send('Data is coming')
-})
+app.get("/", (req, res) => {
+  res.send("Data is coming");
+});
 
-app.listen(port, () =>{
-    console.log('app running on port :', port)
-})
+app.listen(port, () => {
+  console.log("app running on port :", port);
+});
