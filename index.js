2ea8fb5e3dcf8f7f2666c2518e0a6b4bea7917de
omlan99 <omlan10616@gmail.com>
2025-01-19 18:56:00 +0600

    added menu post api

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ async function run() {
 
     // middleware
     const verifyToken = (req, res, next) => {
-      console.log("inside the verifyToken", req.headers);
+  
       if (!req.headers.authorization) {
         return res.status(401).send({ message: "forbidden access" });
       }
@@ -79,15 +79,15 @@ async function run() {
       const user = await userCollection.findOne(query)
      let admin = false
       if(user){
-
-        const admin = user?.role === 'admin'
+        
+         admin = user?.role === 'admin'
       }
       res.send({admin})
 
     })
 
 
-    app.get("/users", verifyToken, async (req, res) => {
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
@@ -103,7 +103,7 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-    app.patch("/users/admin/:id", async (req, res) => {
+    app.patch("/users/admin/:id",verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -114,7 +114,7 @@ async function run() {
       const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
-    app.delete("/users/:id", async (req, res) => {
+    app.delete("/users/:id",verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
@@ -125,6 +125,14 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+
+    app.post('/menu', async (req, res) => {
+      const item = req.body
+      console.log(item)
+      const result = await menuCollection.insertOne(item)
+      res.send(result)
+    })
+
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
